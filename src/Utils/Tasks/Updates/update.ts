4215b5f57a2ac40272d86a9e486530f023c2877f
Vic <duveaux1@gmail.com>
2020-08-12 15:26:49 +0200

    Also update Engine

--- src/Utils/Tasks/Updates/update.ts
+++ src/Utils/Tasks/Updates/update.ts
@@ -9,7 +9,7 @@ export default async (task, models) => {
   if (!result.match("up to date")) {
     console.log("Client update found. Installing and recompiling.");
     task.data.state = "Updates found. Installing and recompiling client.";
-    task.data.progress = 25;
+    task.data.progress = 10;
     task.data.done = false;
     task.markModified("data");
     await task.save();
@@ -21,22 +21,38 @@ export default async (task, models) => {
 
   // Step 2: Server
   task.data.state = "Looking for Server updates";
-  task.data.progress = 50;
+  task.data.progress = 25;
   task.markModified("data");
   await task.save();
 
   result = await shell.exec("git -C /AppBox/System/Server pull");
   if (!result.match("up to date")) {
     task.data.state = "Server: Installing dependencies";
-    task.data.progress = 60;
+    task.data.progress = 30;
     task.markModified("data");
     await task.save();
     result = await shell.exec("yarn --cwd ../Server install");
   }
 
-  // Step 3: SiteServer
+  // Step 3: Engine
+  task.data.state = "Looking for Engine updates";
+  task.data.progress = 50;
+  task.markModified("data");
+  await task.save();
+
+  result = await shell.exec("git -C /AppBox/System/Engine pull");
+  if (!result.match("up to date")) {
+    task.data.state = "Engine: Installing dependencies";
+    task.data.progress = 55;
+    task.markModified("data");
+    await task.save();
+    result = await shell.exec("yarn --cwd ../Engine install");
+  }
+
+
+  // Step 4: SiteServer
   task.data.state = "Looking for SiteServer updates";
-  task.data.progress = 70;
+  task.data.progress = 75;
   task.markModified("data");
   await task.save();
 
@@ -49,22 +65,7 @@ export default async (task, models) => {
     result = await shell.exec("yarn --cwd ../Server install");
   }
 
-  // Step 3: SiteServer
-  task.data.state = "Looking for SiteServer updates";
-  task.data.progress = 90;
-  task.markModified("data");
-  await task.save();
-
-  result = await shell.exec("git -C /AppBox/System/SiteServer pull");
-  if (!result.match("up to date")) {
-    task.data.state = "SiteServer: Installing dependencies";
-    task.data.progress = 95;
-    task.markModified("data");
-    await task.save();
-    result = await shell.exec("yarn --cwd ../Server install");
-  }
-
-  // Step 4: Supervisor
+  // Step 5: Supervisor
   task.data.state = "Done";
   task.data.progress = 100;
   task.data.done = true;
