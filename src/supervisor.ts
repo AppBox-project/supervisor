2fb398e9c65704b1f1a2558f5ac6fac9ecf58cc1
Vic van Cooten <duveaux1@gmail.com>
2020-11-10 18:58:13 +0100

    Fix previous commit

--- src/supervisor.ts
+++ src/supervisor.ts
@@ -101,14 +101,14 @@ Axios.get(`http://${process.env.DBURL || "localhost:27017"}/AppBox`)
       };
       models.objects.stream.on("change", (change) => {
         models.objects.model
-          .find({ objectId: "system-task", "data.target": "engine" })
+          .find({ objectId: "system-task", "data.target": "Supervisor" })
           .then((tasks) => {
             processTasks(tasks);
           });
       });
 
       models.objects.model
-        .find({ objectId: "system-task", "data.target": "engine" })
+        .find({ objectId: "system-task", "data.target": "Supervisor" })
         .then((tasks) => {
           processTasks(tasks);
         });
