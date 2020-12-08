cf39daead173ef1ddb88cac08382795ded54d520
Vic van Cooten <duveaux1@gmail.com>
2020-12-08 20:51:22 +0100

    Rewrite registerSystemValues

--- src/Utils/Tasks/General/InstallScript/registerSystemValues.ts
+++ src/Utils/Tasks/General/InstallScript/registerSystemValues.ts
@@ -9,7 +9,9 @@ export const install = (
         types: string[];
         fields;
         overviews: { [key: string]: {} };
+        lists: { [key: string]: {} };
         layouts: { [key: string]: {} };
+        dropdown_options: { [key: string]: { label; key }[] };
       };
     };
     info: {
@@ -23,108 +25,85 @@ export const install = (
   updateTask: (state: string) => void
 ) =>
   new Promise<void>(async (resolve, reject) => {
-    console.log("Registering system values");
-    const values = args.values;
+    console.log(`Adding system values`);
 
-    // People
-    if (values.people) {
-      const people = values.people;
-      const currentPeopleModel = await models.models.model.findOne({
-        key: "people",
+    map(args?.values || {}, async (values, modelKey) => {
+      console.log(`Adding ${modelKey} values`);
+      const originalModel = await models.models.model.findOne({
+        key: modelKey,
       });
-      let peopleModelHasChanged = false;
+      let modelHasChanged = false;
 
       // Fields
-      if (people.fields) {
-        const fields: {
-          [key: string]: { name; type; typeArgs: { type; formula } };
-        } = people.fields;
-
-        map(fields, (field, fieldKey) => {
-          if (!currentPeopleModel.fields[fieldKey]) {
-            console.log(`Adding field ${fieldKey}`);
-            currentPeopleModel.fields[fieldKey] = field;
-            currentPeopleModel.markModified(`fields.${fieldKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+      map(values?.fields || {}, (field, fieldKey) => {
+        if (!originalModel.fields) originalModel.fields = {};
+        if (!originalModel.fields[fieldKey]) {
+          console.log(`Adding ${modelKey} field ${fieldKey}.`);
+          originalModel.lists[fieldKey] = field;
+          originalModel.markModified("lists");
+          modelHasChanged = true;
+        }
+      });
 
       // Overviews
-      if (people.overviews) {
-        const overviews: {
-          [key: string]: {};
-        } = people.overviews;
-
-        map(overviews, (overview, overviewKey) => {
-          if (!currentPeopleModel.overviews[overviewKey]) {
-            console.log(`Adding overview ${overviewKey}`);
-            currentPeopleModel.overviews[overviewKey] = overview;
-            currentPeopleModel.markModified(`overviews.${overviewKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+      map(values?.overviews || {}, (overview, overviewKey) => {
+        if (!originalModel.overviews) originalModel.overviews = {};
+        if (!originalModel.overviews[overviewKey]) {
+          console.log(`Adding ${modelKey} overview ${overviewKey}.`);
+          originalModel.overviews[overviewKey] = overview;
+          originalModel.markModified("overviews");
+          modelHasChanged = true;
+        }
+      });
 
       // Layouts
-      if (people.layouts) {
-        const layouts: {
-          [key: string]: {};
-        } = people.layouts;
+      map(values?.layouts || {}, (layout, layoutKey) => {
+        if (!originalModel.layouts) originalModel.layouts = {};
 
-        map(layouts, (layout, layoutKey) => {
-          if (!currentPeopleModel.layouts[layoutKey]) {
-            console.log(`Adding layout ${layoutKey}`);
-            currentPeopleModel.layouts[layoutKey] = layout;
-            currentPeopleModel.markModified(`layouts.${layoutKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+        if (!originalModel.layouts[layoutKey]) {
+          console.log(`Adding ${modelKey} layout ${layoutKey}.`);
+          originalModel.layouts[layoutKey] = layout;
+          originalModel.markModified("layouts");
+          modelHasChanged = true;
+        }
+      });
 
-      // Types
-      if (people.types) {
-        //@ts-ignore
-        const types: { label: string; key: string }[] = people.types;
-        types.map((type) => {
-          let typeExists = false;
-          currentPeopleModel.fields.types.typeArgs.options.map(
-            (option, key) => {
-              if (option.key === type.key) {
-                typeExists = true;
-              }
+      // Lists
+      map(values?.lists || {}, (list, listKey) => {
+        if (!originalModel.lists) originalModel.lists = {};
+        if (!originalModel.lists[listKey]) {
+          console.log(`Adding ${modelKey} list ${listKey}.`);
+          originalModel.lists[listKey] = list;
+          originalModel.markModified("layouts");
+          modelHasChanged = true;
+        }
+      });
 
-              // If the withList option is selected add a list (if it doesn't exist yet.)
-              if (option.withList) {
-                if (!currentPeopleModel.lists[option.key]) {
-                  currentPeopleModel.lists[option.key] = {
-                    name: option.listName || option.label,
-                    filter: [
-                      {
-                        key: "types",
-                        operator: "equals",
-                        value: option.key,
-                      },
-                    ],
-                  };
-                  currentPeopleModel.markModified(`lists.${option.key}`);
-                }
-              }
-            }
-          );
-          if (!typeExists) {
-            peopleModelHasChanged = true;
-            (currentPeopleModel.fields.types.typeArgs.options as any[]).push(
-              type
+      // Add local options
+      map(values?.dropdown_options || {}, (options, optionsKey) => {
+        // Loop through options
+        options.map((newOption) => {
+          let newOptionAlreadyExists = false;
+          originalModel.fields[optionsKey].typeArgs.options.map((oldOption) => {
+            if (oldOption.key === newOption.key) newOptionAlreadyExists = true;
+          });
+
+          // If it doesn't exist, add it
+          if (!newOptionAlreadyExists) {
+            console.log(
+              `Adding option for ${modelKey}-${optionsKey}: ${newOption.key}`
             );
-            currentPeopleModel.markModified("fields.types.typeArgs.options");
-            console.log(`Added people type ${type.label}`);
+            originalModel.fields[optionsKey].typeArgs.options.push(newOption);
+            originalModel.markModified(`fields.${optionsKey}.typeArgs.options`);
+            modelHasChanged = true;
           }
         });
-      }
+      });
 
-      await currentPeopleModel.save();
-    }
+      if (modelHasChanged) {
+        await originalModel.save();
+      }
+    });
 
     resolve();
   });
@@ -141,6 +120,8 @@ export const update = (
         }[];
         overviews: { [key: string]: {} };
         layouts: { [key: string]: {} };
+        lists: { [key: string]: {} };
+        dropdown_options: { [key: string]: { label; key }[] };
         fields: {
           [key: string]: { name; type; typeArgs: { type; formula } };
         };
@@ -157,115 +138,80 @@ export const update = (
   updateTask: (state: string) => void
 ) =>
   new Promise<void>(async (resolve, reject) => {
-    console.log(`Checking for system value updates`);
-    const values = args.values;
+    console.log(`Updating system values`);
 
-    // People
-    if (values.people) {
-      const people = values.people;
-      const currentPeopleModel = await models.models.model.findOne({
-        key: "people",
+    map(args?.values || {}, async (values, modelKey) => {
+      console.log(`Updating ${modelKey} values`);
+      const originalModel = await models.models.model.findOne({
+        key: modelKey,
       });
-      let peopleModelHasChanged = false;
+      let modelHasChanged = false;
 
       // Fields
-      if (people.fields) {
-        const fields: {
-          [key: string]: { name; type; typeArgs: { type; formula } };
-        } = people.fields;
-
-        map(fields, (field, fieldKey) => {
-          if (!currentPeopleModel.fields[fieldKey]) {
-            console.log(`Adding field ${fieldKey}`);
-            currentPeopleModel.fields[fieldKey] = field;
-            currentPeopleModel.markModified(`fields.${fieldKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+      map(values?.fields || {}, (field, fieldKey) => {
+        if (!originalModel.lists[fieldKey]) {
+          console.log(`Adding ${modelKey} field ${fieldKey}.`);
+          originalModel.lists[fieldKey] = field;
+          originalModel.markModified("lists");
+          modelHasChanged = true;
+        }
+      });
 
       // Overviews
-      if (people.overviews) {
-        const overviews: {
-          [key: string]: {};
-        } = people.overviews;
-
-        map(overviews, (overview, overviewKey) => {
-          if (!currentPeopleModel.overviews[overviewKey]) {
-            console.log(`Adding overview ${overviewKey}`);
-            currentPeopleModel.overviews[overviewKey] = overview;
-            currentPeopleModel.markModified(`overviews.${overviewKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+      map(values?.overviews || {}, (overview, overviewKey) => {
+        if (!originalModel.overviews[overviewKey]) {
+          console.log(`Adding ${modelKey} overview ${overviewKey}.`);
+          originalModel.overviews[overviewKey] = overview;
+          originalModel.markModified("overviews");
+          modelHasChanged = true;
+        }
+      });
 
       // Layouts
-      if (people.layouts) {
-        const layouts: {
-          [key: string]: {};
-        } = people.layouts;
+      map(values?.layouts || {}, (layout, layoutKey) => {
+        if (!originalModel.layouts[layoutKey]) {
+          console.log(`Adding ${modelKey} layout ${layoutKey}.`);
+          originalModel.layouts[layoutKey] = layout;
+          originalModel.markModified("layouts");
+          modelHasChanged = true;
+        }
+      });
 
-        map(layouts, (layout, layoutKey) => {
-          if (!currentPeopleModel.layouts[layoutKey]) {
-            console.log(`Adding layout ${layoutKey}`);
-            currentPeopleModel.layouts[layoutKey] = layout;
-            currentPeopleModel.markModified(`layouts.${layoutKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+      // Lists
+      map(values?.lists || {}, (list, listKey) => {
+        if (!originalModel.lists[listKey]) {
+          console.log(`Adding ${modelKey} list ${listKey}.`);
+          originalModel.lists[listKey] = list;
+          originalModel.markModified("layouts");
+          modelHasChanged = true;
+        }
+      });
 
-      // Types
-      if (people.types) {
-        //@ts-ignore
-        const types: {
-          label: string;
-          key: string;
-          withList?: boolean;
-          listName?: string;
-        }[] = people.types;
-        types.map((type) => {
-          let typeExists = false;
-          currentPeopleModel.fields.types.typeArgs.options.map(
-            (option, key) => {
-              if (option.key === type.key) {
-                typeExists = true;
-              }
+      // Add local options
+      map(values?.dropdown_options || {}, (options, optionsKey) => {
+        // Loop through options
+        options.map((newOption) => {
+          let newOptionAlreadyExists = false;
+          originalModel.fields[optionsKey].typeArgs.options.map((oldOption) => {
+            if (oldOption.key === newOption.key) newOptionAlreadyExists = true;
+          });
 
-              // If the withList option is selected add a list (if it doesn't exist yet.)
-              if (type.withList) {
-                if (!(currentPeopleModel.lists || {})[type.key]) {
-                  currentPeopleModel.lists[type.key] = {
-                    name: type.listName || type.label,
-                    filter: [
-                      {
-                        key: "types",
-                        operator: "equals",
-                        value: type.key,
-                      },
-                    ],
-                  };
-                  currentPeopleModel.markModified(`lists.${type.key}`);
-                  peopleModelHasChanged = true;
-                }
-              }
-            }
-          );
-
-          if (!typeExists) {
-            peopleModelHasChanged = true;
-            (currentPeopleModel.fields.types.typeArgs.options as any[]).push(
-              type
+          // If it doesn't exist, add it
+          if (!newOptionAlreadyExists) {
+            console.log(
+              `Adding option for ${modelKey}-${optionsKey}: ${newOption.key}`
             );
-            currentPeopleModel.markModified("fields.types.typeArgs.options");
-            console.log(`Added people type ${type.label}`);
+            originalModel.fields[optionsKey].typeArgs.options.push(newOption);
+            originalModel.markModified(`fields.${optionsKey}.typeArgs.options`);
+            modelHasChanged = true;
           }
         });
-      }
+      });
 
-      if (peopleModelHasChanged) await currentPeopleModel.save();
-    }
+      if (modelHasChanged) {
+        await originalModel.save();
+      }
+    });
 
     resolve();
   });
@@ -275,14 +221,11 @@ export const uninstall = (
   args: {
     values: {
       people?: {
-        types: {
-          label: string;
-          key: string;
-          withList?: boolean;
-          listName?: string;
-        }[];
         overviews: { [key: string]: {} };
         layouts: { [key: string]: {} };
+        lists: { [key: string]: {} };
+        dropdown_options: { [key: string]: { label; key }[] };
+
         fields: {
           [key: string]: { name; type; typeArgs: { type; formula } };
         };
@@ -299,96 +242,79 @@ export const uninstall = (
   updateTask: (state: string) => void
 ) =>
   new Promise<void>(async (resolve, reject) => {
-    console.log(`Deleting system value updates`);
-    const values = args.values;
-    // People
-    if (values.people) {
-      const people = values.people;
-      const currentPeopleModel = await models.models.model.findOne({
-        key: "people",
+    console.log(`Deleting values that app registered in system`);
+
+    map(args?.values || {}, async (values, modelKey) => {
+      console.log(`Deleting ${modelKey} values`);
+      const originalModel = await models.models.model.findOne({
+        key: modelKey,
       });
-      let peopleModelHasChanged = false;
+      let modelHasChanged = false;
 
       // Fields
-      if (people.fields) {
-        const fields: {
-          [key: string]: { name; type; typeArgs: { type; formula } };
-        } = people.fields;
-
-        map(fields, (field, fieldKey) => {
-          if (currentPeopleModel.fields[fieldKey]) {
-            console.log(`Deleting field ${fieldKey}`);
-            delete currentPeopleModel.fields[fieldKey];
-            currentPeopleModel.markModified(`fields.${fieldKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+      map(values?.fields || {}, (field, fieldKey) => {
+        if (originalModel.fields[fieldKey]) {
+          console.log(`Deleting ${modelKey} field ${fieldKey}.`);
+          delete originalModel.fields[fieldKey];
+          originalModel.markModified("lists");
+          modelHasChanged = true;
+        }
+      });
 
       // Overviews
-      if (people.overviews) {
-        const overviews: {
-          [key: string]: {};
-        } = people.overviews;
-
-        map(overviews, (overview, overviewKey) => {
-          if (currentPeopleModel.overviews[overviewKey]) {
-            console.log(`Deleting overview ${overviewKey}`);
-            delete currentPeopleModel.overviews[overviewKey];
-            currentPeopleModel.markModified(`overviews.${overviewKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+      map(values?.overviews || {}, (overview, overviewKey) => {
+        if (originalModel.overviews[overviewKey]) {
+          console.log(`Deleting ${modelKey} overview ${overviewKey}.`);
+          delete originalModel.overviews[overviewKey];
+          originalModel.markModified("overviews");
+          modelHasChanged = true;
+        }
+      });
 
       // Layouts
-      if (people.layouts) {
-        const layouts: {
-          [key: string]: {};
-        } = people.layouts;
+      map(values?.layouts || {}, (layout, layoutKey) => {
+        if (originalModel.layouts[layoutKey]) {
+          console.log(`Deleting ${modelKey} layout ${layoutKey}.`);
+          delete originalModel.layouts[layoutKey];
+          originalModel.markModified("layouts");
+          modelHasChanged = true;
+        }
+      });
 
-        map(layouts, (layout, layoutKey) => {
-          if (currentPeopleModel.layouts[layoutKey]) {
-            console.log(`Deleting layout ${layoutKey}`);
-            delete currentPeopleModel.layouts[layoutKey];
-            currentPeopleModel.markModified(`layouts.${layoutKey}`);
-            peopleModelHasChanged = true;
-          }
-        });
-      }
+      // Lists
+      map(values?.lists || {}, (list, listKey) => {
+        if ((originalModel.lists || {})[listKey]) {
+          console.log(`Deleting ${modelKey} list ${listKey}.`);
+          delete originalModel.lists[listKey];
+          originalModel.markModified("lists");
+          modelHasChanged = true;
+        }
+      });
 
-      // Types
-      if (people.types) {
-        //@ts-ignore
-        const types: {
-          label: string;
-          key: string;
-          withList?: boolean;
-          listName?: string;
-        }[] = people.types;
-        types.map((type) => {
-          currentPeopleModel.fields.types.typeArgs.options.splice(
-            findIndex(
-              currentPeopleModel.fields.types.typeArgs.options,
-              (o: { key; label }) => o.key === type.key
-            ),
-            1
-          );
-
-          currentPeopleModel.markModified("fields.types.typeArgs.options");
-
-          if (type.withList) {
-            // This type came with a list. Remove it.
-            if ((currentPeopleModel.lists || [])[type.key]) {
-              delete currentPeopleModel.lists[type.key];
-              currentPeopleModel.markModified("fields.lists");
+      // Dropdown options
+      map(values?.dropdown_options || {}, (options, optionsKey) => {
+        // Loop through options
+        options.map((newOption) => {
+          originalModel.fields[optionsKey].typeArgs.options.map(
+            (oldOption, oldOptionIndex) => {
+              if (oldOption.key === newOption.key) {
+                console.log(
+                  `Deleting option for ${modelKey}-${optionsKey}: ${newOption.key}`
+                );
+                originalModel.fields[optionsKey].typeArgs.options.splice(
+                  oldOptionIndex,
+                  1
+                );
+              }
             }
-          }
+          );
         });
-      }
+      });
 
-      await currentPeopleModel.save();
-    }
+      if (modelHasChanged) {
+        await originalModel.save();
+      }
+    });
 
     resolve();
   });
