18a6bcd664aaf7482616fec9f8695d5f6435cd82
Vic van Cooten <duveaux1@gmail.com>
2021-02-17 17:15:36 +0100

    Install collections

--- src/Utils/Tasks/General/installApp.ts
+++ src/Utils/Tasks/General/installApp.ts
@@ -15,49 +15,107 @@ export default async (oldTask, models) => {
   const task = await models.objects.model.findOne({ _id: oldTask._id });
 
   // Announce that supervisor has started the work.
-  await updateTask(task, 5, "Downloading");
+  await updateTask(task, 5, "Getting ready");
 
-  // Download
-  result = await shell.exec(
-    `git -C /AppBox/System/Temp/Apps clone ${oldTask.data.arguments.app.data.repository} ${oldTask.data.arguments.app.data.key}`
-  );
+  if (oldTask.data.arguments.app.data.repository) {
+    // Download
+    result = await shell.exec(
+      `git -C /AppBox/System/Temp/Apps clone ${oldTask.data.arguments.app.data.repository} ${oldTask.data.arguments.app.data.key}`
+    );
 
-  if (result.code === 128) {
-    // Folder was alread there.
-    console.log("Old code found. Checking updates.");
+    if (result.code === 128) {
+      // Folder was alread there.
+      console.log("Old code found. Checking updates.");
 
-    await updateTask(task, 8, "Old code found. Checking updates.");
-    await shell.exec(`git -C ${dir} pull`);
-  }
-
-  if (fs.existsSync(`${dir}/install.yml`)) {
-    // Execute installscript
-    await updateTask(task, 20, "Running pre-install scripts");
-
-    const file = fs.readFileSync(`${dir}/install.yml`, "utf8");
-    const installScript = YAML.parse(file);
-
-    let scriptVersion = "script";
-    // In case there's multiple versions based on the choices the user made, pick the right script
-    if (installScript.versions) {
-      installScript.versions.map((version) => {
-        let match = true;
-        map(version.criteria, (value, key) => {
-          if (oldTask.data.arguments.choices[key] != value) {
-            match = false;
-          }
-        });
-
-        if (match) scriptVersion = version.script;
-      });
+      await updateTask(task, 8, "Old code found. Checking updates.");
+      await shell.exec(`git -C ${dir} pull`);
     }
 
-    // Execute the install script
-    const script = installScript[scriptVersion || "script"];
+    if (fs.existsSync(`${dir}/install.yml`)) {
+      // Execute installscript
+      await updateTask(task, 20, "Running pre-install scripts");
 
-    const data = installScript.data;
+      const file = fs.readFileSync(`${dir}/install.yml`, "utf8");
+      const installScript = YAML.parse(file);
+
+      let scriptVersion = "script";
+      // In case there's multiple versions based on the choices the user made, pick the right script
+      if (installScript.versions) {
+        installScript.versions.map((version) => {
+          let match = true;
+          map(version.criteria, (value, key) => {
+            if (oldTask.data.arguments.choices[key] != value) {
+              match = false;
+            }
+          });
+
+          if (match) scriptVersion = version.script;
+        });
+      }
+
+      // Execute the install script
+      const script = installScript[scriptVersion || "script"];
+
+      const data = installScript.data;
+      let currentPercentage = 20;
+
+      const stepSize = 80 / (script || []).length;
+
+      await ((script as { action: string }[]) || []).reduce(
+        async (prev, step) => {
+          await prev;
+          let action;
+          let args = {
+            info: script.info,
+            key: oldTask.data.arguments.app.data.key,
+            choices: oldTask.data.arguments.choices,
+          };
+          if (typeof step === "object") {
+            action = step.action;
+            args = { ...args, ...step };
+          } else {
+            action = step;
+          }
+
+          if (!installScriptFunctions[action]) {
+            console.error(`Install script step ${action} not found.`);
+            return false;
+          }
+          return await installScriptFunctions[action](
+            args,
+            models,
+            data,
+            async (state: string) => {
+              currentPercentage += stepSize;
+              await updateTask(task, currentPercentage, state);
+            }
+          );
+        },
+        (script || [])[0]
+      );
+      console.log("Done");
+
+      // Done following install script
+      await updateTask(task, 100, "Installation complete!");
+    } else {
+      task.data.progress = -1;
+      task.data.state = "Install script missing";
+      task.data.error = true;
+      task.markModified("data.state");
+      task.markModified("data.error");
+      task.markModified("data.progress");
+      await task.save();
+    }
+  } else {
+    const app = oldTask.data.arguments.app.data.collection_data;
+    const collection_data = app.collection_data;
+    const script = collection_data.installScript.script;
+    const data = collection_data.installScript.data;
+
+    console.log("Installing collection based app.", data);
+    console.log(data.models);
+
     let currentPercentage = 20;
-
     const stepSize = 80 / (script || []).length;
 
     await ((script as { action: string }[]) || []).reduce(
@@ -65,9 +123,7 @@ export default async (oldTask, models) => {
         await prev;
         let action;
         let args = {
-          info: script.info,
           key: oldTask.data.arguments.app.data.key,
-          choices: oldTask.data.arguments.choices,
         };
         if (typeof step === "object") {
           action = step.action;
@@ -92,18 +148,11 @@ export default async (oldTask, models) => {
       },
       (script || [])[0]
     );
-    console.log("Done");
 
-    // Done following install script
-    await updateTask(task, 100, "Installation complete!");
-  } else {
-    task.data.progress = -1;
-    task.data.state = "Install script missing";
-    task.data.error = true;
-    task.markModified("data.state");
-    task.markModified("data.error");
-    task.markModified("data.progress");
-    await task.save();
+    console.log("Finished script. Registering app");
+    await updateTask(task, 95, "Registering app");
+    await new models.objects.model({ objectId: "apps", data: app }).save();
+    await updateTask(task, 100, "Collection installed");
   }
 };
 
